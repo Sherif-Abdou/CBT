e8dd74d0306b7f1ee0df193976d003718d12655e
Sherif Abdou <sherif_abdou@outlook.com>
2020-07-30 12:30:26 -0700

    Added Stub Website with React and Redu

---
+++ src/components/MainWindow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainWindow() {
+    return (<h1>Peoples</h1>);
+}
+
+export default MainWindow;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MainWindow from "./components/MainWindow";
+
+
+console.log("Hello Land");
+ReactDOM.render(<MainWindow />, document.getElementById("root"));
